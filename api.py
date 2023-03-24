776928abd64b6fc0fcc4b4645fed2da27408cea5
Vibhor Jain <vibhor.jain@canavibs.com>
2023-03-24 18:40:22 +0530

    added code for instrumentation using prometheus

--- api.py
+++ api.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort, marshal, fields
 from flask_cors import CORS
+from prometheus_flask_exporter import RESTfulPrometheusMetrics
 
 # Initialize Flask
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
+metrics = RESTfulPrometheusMetrics(app, api)
+
+metrics.info('app_info', 'Application info', version='1.0', app_name='devops-bookstore-api')
 
 # A List of Dicts to store all of the books
 books = [{
@@ -47,8 +52,9 @@ class BookList(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
 
+    @metrics.summary('requests_by_status', 'Request latencies by status', labels={'status': lambda r: r.status_code})
     def get(self):
-        return{"books": [marshal(book, bookFields) for book in books]}
+        return{"books": [marshal(book, bookFields) for book in books]}, 200
 
 
 api.add_resource(BookList, "/books")
